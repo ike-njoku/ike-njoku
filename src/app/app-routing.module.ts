04d908db5d34220fc4e4034fc284b60e8937b6e5
ike-njoku <ikenjokudc@gmail.com>
2020-12-15 20:34:57 -0800

    added routes for index and portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // index component
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'index',redirectTo:'/home'},
+  {path:'home',component:IndexComponent},
+  // portfolio
+  {path:'portfolio',component:PortfolioComponent}
+  // page not found (404)
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
