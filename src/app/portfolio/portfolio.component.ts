759e5376e7491677adaee4b75219d4c1a7f18373
ike-njoku <ikenjokudc@gmail.com>
2020-12-16 01:46:17 -0800

    Updated portfolio component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  // items to count
+  itemsToCount = [
+    {name:'Clients', runningValue:0 ,stopValue:42 },
+    {name:'Projects', runningValue:0 ,stopValue:52 },
+    {name:'Articles', runningValue:0 ,stopValue:174 },
+  ]
+
+  // count each item pair
+  count(){
+    this.itemsToCount.forEach((item)=>{
+      // range checks
+      if(item.runningValue == item.stopValue) return
+      // increment the running value
+      item.runningValue++;
+    })
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(()=>this.count(),100);
   }
 
 }
