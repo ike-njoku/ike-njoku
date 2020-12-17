2bae10e9b483ed00f8323006ed6bced58897d82b
ike-njoku <ikenjokudc@gmail.com>
2020-12-17 05:18:39 -0800

    created project interface

---
+++ src/app/interfaces/project-interface.ts
@@ -0,0 +1,9 @@
+export interface Project{
+    name: string;
+    fullName: string;
+    private: boolean;
+    url:string;
+    avatarUrl?: string;
+    languages?: string[];
+    contributors?: string[];
+}
