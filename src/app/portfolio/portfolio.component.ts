5cdfb8637789792d451ff98e3893b1360ab7be6a
ike-njoku <ikenjokudc@gmail.com>
2020-12-28 17:45:03 -0800

    renderer, view child, elementRef in portfolio.component.ts

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import { PortfolioService } from '../services/portfolio.service';
 
 
@@ -8,6 +8,8 @@ import { PortfolioService } from '../services/portfolio.service';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+
+  @ViewChild('projectsGrid')projectsGrid:ElementRef;
 
   // items to count
   itemsToCount = [
@@ -41,10 +43,21 @@ export class PortfolioComponent implements OnInit {
     },(error)=>window.alert(error))
   }
 
-  constructor(private portFolioService : PortfolioService) { }
+  constructor(
+    private portFolioService : PortfolioService,
+    private renderer : Renderer2
+  ) { }
 
   ngOnInit(): void {
     setInterval(()=>this.count(),70);
     this.getGitHubRepos();
+  }
+
+  // ngAfterViewInit
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log(this.projectsGrid.nativeElement.children);
+    
   }
 }
