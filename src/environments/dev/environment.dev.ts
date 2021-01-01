f155d8d7b1d50aa9f0a2d5d4a28cbeb78728b2e6
ike-njoku <ikenjokudc@gmail.com>
2021-01-01 01:14:36 -0800

    updates

---
+++ src/environments/dev/environment.dev.ts
@@ -0,0 +1,10 @@
+// dev vavariables
+
+export const environment = {
+    production: false,
+    // url to fetch github repos
+    gitHubApiUrl:  'api/gitHubRepos',
+    // the mode i'm fiddling with
+    mode:'(Dev-Mode)'
+};
+  
