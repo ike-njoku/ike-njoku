fec9a17878e2aeb028b03d3da54bc728b4d0db0d
ike-njoku <ikenjokudc@gmail.com>
2021-01-04 11:40:07 -0800

    updates

---
+++ src/environments/dev/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiBase: '',
+    gitHubApiBase:'',
+    env: 'dev'
+};
---
+++ src/environments/prod/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiBase: 'http://prod-server:2080/app/',
+    gitHubApiBase:'https://api.github.com/users/ike-njoku/repos',
+    env: 'prod'
+};
