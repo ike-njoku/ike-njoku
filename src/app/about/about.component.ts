a414e3c57f25bf1e064c2b00691ad9e3daf2f714
ike-njoku <ikenjokudc@gmail.com>
2021-01-04 18:42:35 -0800

    updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 
 @Component({
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
+
+
 export class AboutComponent implements OnInit {
+
+  @ViewChild('heading')contentHeading: ElementRef;
+  @ViewChild('body')contentBody: ElementRef;
 
   contents = [
     {title:" Who I am ", body:" My Name Is Ike-Njoku David. I am a software Engineer and freelance Web Developer with quite some Experience. Over the years, I have become more than just a programmer; teaching people to code both in class rooms and through articles!. I love to write, play the guitar, basket ball and travel. I love photography  "},
@@ -18,6 +24,10 @@ export class AboutComponent implements OnInit {
   currentContentIndex = 0;
   // current Content
   currentContent = this.contents[this.currentContentIndex];
+
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
   // jump to content
   goToContent(content){
@@ -31,16 +41,25 @@ export class AboutComponent implements OnInit {
     }
   }
 
-  constructor(
-  ) { }
+  // add animataions
+  addAnimation(animationName){
+    // remove the animation first
+    this.renderer.removeClass(this.contentHeading.nativeElement,animationName);
+    this.renderer.removeClass(this.contentBody.nativeElement,animationName);
+
+    // wait a small while before re-adding the animation
+    setTimeout(()=>{
+      this.renderer.addClass(this.contentBody.nativeElement,animationName);
+      this.renderer.addClass(this.contentHeading.nativeElement,animationName);
+    },5)
+
+  }
+
 
   ngOnInit(): void {
     this.currentContent = this.contents[this.currentContentIndex];
   }
 
-  ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
-  }
+ 
 
 }
