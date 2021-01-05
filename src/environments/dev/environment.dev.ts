fd6c183e6f895983078b6db3be13052a95fd1987
ike-njoku <ikenjokudc@gmail.com>
2021-01-04 19:10:49 -0800

    added mock repos json file

--- src/environments/dev/environment.dev.ts
+++ src/environments/dev/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiBase: '',
-    gitHubApiBase:'',
+    gitHubApiBase:'file://wsl%24/Ubuntu/home/ikenjokudc/ike-njoku/src/environments/dev/mock-repos.json',
     env: 'dev'
 };
