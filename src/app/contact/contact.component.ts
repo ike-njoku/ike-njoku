2ac058b31dad876dfcbb4cd55c142aeb19e940b2
ike-njoku <ikenjokudc@gmail.com>
2021-01-21 20:52:48 -0800

    updates

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
   constructor() { }
+  displayMailBox: boolean = true;
+  
+  showNailBox(){
+    if(this.displayMailBox == true)this.displayMailBox = false;
+    else this.displayMailBox = false;
+  }
 
   ngOnInit(): void {
+
   }
 
 }
